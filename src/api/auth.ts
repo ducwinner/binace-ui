1dd5fc330f6dfe7b9aec1fb8b6b7d7820b611ed3
Nguyễn Văn Đức <nguyenvanduc19102000@gmail.com>
2022-08-08 14:58:45 +0700

    fix Api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-// const local = "http://localhost:4666/api"
-const dev = "https://cors-anywhere.herokuapp.com/https://duc-backend-nodejs.herokuapp.com/api"
+const local = "http://localhost:4555/api"
+const dev = "https://duc-backend-nodejs.herokuapp.com/api"
 const authAxios = axios.create({
   baseURL: dev,
   headers: { 'content-type': 'application/json',
